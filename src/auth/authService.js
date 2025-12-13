c1b1349f7b4d3072a74508465f021e78557534e1
luislopezmuy <luislopezmuy@gmail.com>
2025-12-12 22:27:53 -0600

    Se arregla el login para que funcione con vercel

--- src/auth/authService.js
+++ src/auth/authService.js
@@ -2,7 +2,10 @@ import supabase from '../supabase/client';
 
 export const signInWithGoogle = async () => {
   return await supabase.auth.signInWithOAuth({
-    provider: 'google'
+    provider: 'google',
+    options: {
+      redirectTo: window.location.origin
+    }
   })
 }
 
